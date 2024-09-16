9dae6f91f2782be82192e650549ebd21195af083
ajlunis <ajlunis@gmail.com>
2024-09-16 13:30:43 +0000

    modified:   public/service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,6 @@
 const CACHE_NAME = 'gopro-wifi-enabler-cache-v1';
 const urlsToCache = [
   '/gopro-wifi-enabler/',
-  '/gopro-wifi-enabler/assets/index-Bf6tBU-m.css',
-  '/gopro-wifi-enabler/assets/index-DzkS1RDX.js',
   '/gopro-wifi-enabler/manifest.json',
   '/gopro-wifi-enabler/favicon.ico'
 ];
@@ -14,6 +12,22 @@ self.addEventListener('install', (event) => {
       .catch(error => console.error('Failed to cache', error))
   );
 });
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.endsWith('.css') || event.request.url.endsWith('.js')) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request).then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+  }
+});
+
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
